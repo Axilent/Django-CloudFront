d3ff54fadc6110d390c8a064b831d74a15a70aca
Loren Davie <loren@axilent.com>
2012-03-20 17:11:53 -0400

    More required settings.

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
-Django Cloudfront
+Django CloudFront
 =================
 
 Django Cloudfront is a django cache backend that uses AWS Cloudfront (an AWS integrated CDN) as a backend.
 
 Recently AWS announced they were [reducing the minimum TTL for Cloudfront](http://aws.amazon.com/about-aws/whats-new/2012/03/19/amazon-cloudfront-lowers-minimum-expiration-period/).  This makes CloudFront suitable to be an arbitrary, industrial strength caching mechanism.  Creating a new caching backend means Django apps that use the built-in caching framework will be able to use CloudFront without further modification to Django apps.
 
+Required Settings
+-----------------
+
+*	`CLOUDFRONT_BUCKET`: The name of the AWS CloudFront bucket to use.
+*	`AWS_ACCESS_KEY_ID`: The access key id for your AWS account.
+*	`AWS_SECRET_ACCESS_KEY`: The secret access key for your AWS account.
--- axilent/cache/cloudfront.py
+++ axilent/cache/cloudfront.py
@@ -3,12 +3,18 @@ Cache backend for Django that uses AWS Cloudfront.
 """
 from django.core.cache.backends.base import BaseCache
 
-# TODO - this is just a copy from DummyCache out of Django with the name changed.
-# This illustrates the contract that needs to be implemented.
+import boto
+from django.conf import settings
+
+aws_access_key_id = settings.AWS_ACCESS_KEY_ID
+aws_secret_access_key = settings.AWS_SECRET_ACCESS_KEY
+bucket = settings.CLOUDFRONT_BUCKET
 
 class CloudFrontCache(BaseCache):
     def __init__(self, host, *args, **kwargs):
         BaseCache.__init__(self, *args, **kwargs)
+        self.conn = boto.connect_cloudfront(aws_access_key_id=aws_access_key_id,aws_secret_access_key=aws_secret_access_key) # establish cloudfront connection
+        self.distro = self.conn.create_distribution(origin='%s.s3.amazonaws.com' % bucket,enabled=True)
 
     def add(self, key, value, timeout=None, version=None):
         key = self.make_key(key, version=version)
